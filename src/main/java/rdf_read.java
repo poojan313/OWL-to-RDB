40ab84c2e1b217cf16541aacfce21e0e71f02d78
Poojan Khatri <Poojan.Khatri@iiitb.org>
2021-03-31 18:57:17 +0530

    Parsing the entire owl file

--- src/main/java/rdf_read.java
+++ src/main/java/rdf_read.java
@@ -1,7 +1,4 @@
-import org.apache.jena.ontology.AllDifferent;
-import org.apache.jena.ontology.OntClass;
-import org.apache.jena.ontology.OntModel;
-import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.ontology.*;
 import org.apache.jena.rdf.model.*;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.util.FileManager;
@@ -15,7 +12,7 @@ public class rdf_read {
         OntModel model = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM);
         String ns = "http://www.w3.org/2002/07/owl#";
         // use the RDFDataMgr to find the input file
-        InputStream in = RDFDataMgr.open( "src/main/resources/First.owl" );
+        InputStream in = RDFDataMgr.open("src/main/resources/First.owl");
         if (in == null) {
             throw new IllegalArgumentException(
                     "File: sample.rdf +  not found");
@@ -24,44 +21,88 @@ public class rdf_read {
 // read the RDF/XML file
         model.read(in, null);
 
-        ExtendedIterator<OntClass> iterator = model.listClasses();
-        while (iterator.hasNext()){
-            OntClass ontClass = (OntClass) iterator.next();
-            System.out.println("Class is : " + ontClass.toString());
-            if(ontClass.hasSubClass()){
-                System.out.println("SubClass is : " + ontClass.getSubClass());
-            }
-            if(ontClass.hasSuperClass()){
-                System.out.println("SuperClass is : " + ontClass.getSuperClass());
-            }
-        }
-
-
-
-
-
-
-
-
-
-
-//        StmtIterator iterator = model.listStatements();
-//        while (iterator.hasNext())
-//        {
-//            System.out.println("*************************************");
-//            Statement statement = iterator.nextStatement();
-//            Resource subject = statement.getSubject();
-//            Property predicate = statement.getPredicate();
-//            RDFNode object = statement.getObject();
-//            System.out.println("Subject is: " + subject.toString());
-//            System.out.println("Predicate is: " + predicate.toString());
-//            System.out.println("Object is: " + object.toString());
-//            System.out.println("Class is: " + statement.getClass().toString());
-//            if(object instanceof OntClass){
-//
+//        ExtendedIterator<OntClass> iterator = model.listClasses();
+//        while (iterator.hasNext()) {
+//            OntClass ontClass = (OntClass) iterator.next();
+////            ExtendedIterator<? extends OntResource> iterator1 = ontClass.listInstances();
+////            while (iterator1.hasNext()){
+////                StmtIterator iterator2 = iterator1.next().getModel().listStatements();
+////                while (iterator2.hasNext()){
+////                    System.out.println(iterator2.);
+////                }
+////            }
+//            System.out.println("Class is : " + ontClass.toString());
+//            if (ontClass.hasSubClass()) {
+//                System.out.println("SubClass is : " + ontClass.getSubClass());
 //            }
+//            if (ontClass.hasSuperClass()) {
+//                System.out.println("SuperClass is : " + ontClass.getSuperClass());
+//            }
+//        }
+
+//        ExtendedIterator<ObjectProperty> nodeIterator = model.listObjectProperties();
+//        while (nodeIterator.hasNext()) {
+//            ObjectProperty objectProperty = (ObjectProperty) nodeIterator.next();
+//            System.out.println("Object property is : " + objectProperty.toString());
+////            System.out.println("Domain is: " + objectProperty.getDomain().toString());
+//            ExtendedIterator<? extends OntProperty> extendedIterator = objectProperty.listInverseOf();
+//            if (extendedIterator.hasNext()) {
+//                System.out.println("Inverse Object property of: " + objectProperty.getInverseOf().toString());
+//                System.out.println("Domain is: " + objectProperty.getInverseOf().getRange().toString());
+//                System.out.println("Range is: " + objectProperty.getInverseOf().getDomain().toString());
+//            } else {
+//                System.out.println("Domain is: " + objectProperty.getDomain().toString());
+//                System.out.println("Range is: " + objectProperty.getRange().toString());
+//            }
+//
+//        }
+//        ExtendedIterator<DatatypeProperty> iterator1 = model.listDatatypeProperties();
+//        while (iterator1.hasNext()) {
+//            DatatypeProperty datatypeProperty = (DatatypeProperty) iterator1.next();
+//            System.out.println("Datatype property is: " + datatypeProperty.toString());
+//            System.out.println("Domain is: " + datatypeProperty.getDomain().toString());
+//            System.out.println("Range is: " + datatypeProperty.getRange().toString());
+//        }
+////        ExtendedIterator<Individual> individualExtendedIterator = model.listIndividuals();
+////        while (individualExtendedIterator.hasNext()){
+////            System.out.println(individualExtendedIterator.next().toString());
+////        }
+//        ExtendedIterator<AllDifferent> allDifferentExtendedIterator = model.listAllDifferent();
+//        while (allDifferentExtendedIterator.hasNext()){
+//            RDFList members = allDifferentExtendedIterator.next().getDistinctMembers();
+//            System.out.println(members.toString());
+//        }
+        StmtIterator iterator2 = model.listStatements();
+        while (iterator2.hasNext()) {
+            System.out.println("*************************************");
+            Statement statement = iterator2.nextStatement();
+            Resource subject = statement.getSubject();
+            Property predicate = statement.getPredicate();
+            RDFNode object = statement.getObject();
+//            System.out.println(statement.getProperty(predicate).toString());
+            System.out.println("Subject is: " + subject.getLocalName());
+            System.out.println("Predicate is: " + predicate.getLocalName());
+            if(object.isLiteral())
+            {
+//                System.out.println("These values have to be added in the database");
+                System.out.println("Object is: " + object.asLiteral().getString());
+            }
+
+            else if(object.isResource())
+            {
+//                StmtIterator iterator = subject.listProperties();
+//                while (iterator.hasNext()){
+//                    System.out.println("Properties for "+ subject.getLocalName()+ " is: "+ iterator.next().getSubject().getLocalName());
+//                }
+                System.out.println("Object asResource is: " + object.asResource().getLocalName());
+            }
+
+            else
+                System.out.println("Object is: " + object.toString());
+
         }
 // write it to standard out
 //        model.write(System.out);
     }
+}
 
